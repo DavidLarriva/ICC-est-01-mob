29f89102a341ecea937ac19c412462170c818365
David Larriva <dalarrivac@gmail.com>
2024-09-25 11:45:50 -0500

    first commit

---
+++ src/main/java/ups/edu/ec/icc_est_01_mob/ICC_est_01_mob.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ups.edu.ec.icc_est_01_mob;
+
+/**
+ *
+ * @author dalar
+ */
+public class ICC_est_01_mob {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
